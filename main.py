cb37f809af7441eb4bfad5e1ac26be16f54e222a
chasuhui <139537461+chasuhui@users.noreply.github.com>
2023-08-10 11:30:28 +0900

    배포 : dataspace 배포 준비
    DB 테이블 생성 로직 추가

--- main.py
+++ main.py
@@ -11,6 +11,18 @@ cur = con.cursor()
 
 app = FastAPI()
 
+cur.execute(f"""
+            CREATE TABLE IF NOT EXISTS items (
+                id INTEGER PRIMARY KEY,
+                title TEXT NOT NULL,
+                image BLOB
+                price INTEGER NOT NULL,
+                description TEXT,
+                place TEXT NOT NULL,
+                insertAt INTEGER NOT NULL
+            );
+            """)
+
 
 @app.post('/itmes')
 async def create_item(image: UploadFile,
